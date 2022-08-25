b613ce437fe8e8b6872d06fc6a4350b372f6ba46
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-08-25 11:09:09 -0300

    Objeto sigue cursor

---
+++ Assets/GetMousePosition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetMousePosition : MonoBehaviour
+{
+    public Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+        transform.position = mousePosition;
+    }
+}
