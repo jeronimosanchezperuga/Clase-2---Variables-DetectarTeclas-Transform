0758565acbaf48e87e25f0af9b11daa980044cd9
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-08-25 11:35:01 -0300

    Desplazar elementos UI

---
+++ Assets/MenuDesplazable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuDesplazable : MonoBehaviour
+{
+    public RectTransform menuTransform;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MiFuncion();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MiFuncion()
+    {
+        Debug.Log("Ejecuté mi función");
+    }
+
+    public void DesplazarMenuAIzq()
+    {
+        menuTransform.position += new Vector3(-180, 0, 0);
+    }
+
+}
