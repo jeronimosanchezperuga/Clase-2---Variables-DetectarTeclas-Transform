5bd4f70127e7ea47dea828ffe522a1219e971679
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-08-25 11:58:06 -0300

    Dejar pasar tiempo entre loops con Coroutines

---
+++ Assets/DejarPasarUnFrameEntreLoops.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DejarPasarUnFrameEntreLoops : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DejarPasarUnFrame());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator DejarPasarUnFrame()
+    {
+        for (int i = 0; i <= 1000; i++)
+        {
+            Debug.Log(Time.time);
+            yield return new WaitForSeconds(0.001f);
+        }
+    }
+}
